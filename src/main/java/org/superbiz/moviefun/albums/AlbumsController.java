0988eeb4d6f1e98781f49a3dcbcd2e4e96bdb906
ryubato <ryubato7@gmail.com>
2019-09-20 15:50:00 +0900

    removing writes to the file system

--- src/main/java/org/superbiz/moviefun/albums/AlbumsController.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumsController.java
@@ -1,35 +1,35 @@
 package org.superbiz.moviefun.albums;
 
-import org.apache.tika.Tika;
+import org.apache.tika.io.IOUtils;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.superbiz.moviefun.blobstore.Blob;
+import org.superbiz.moviefun.blobstore.BlobStore;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
+import java.util.Optional;
 
-import static java.lang.ClassLoader.getSystemResource;
 import static java.lang.String.format;
-import static java.nio.file.Files.readAllBytes;
+import static org.springframework.util.MimeTypeUtils.IMAGE_JPEG_VALUE;
 
 @Controller
 @RequestMapping("/albums")
 public class AlbumsController {
 
     private final AlbumsBean albumsBean;
+    private final BlobStore blobStore;
 
-    public AlbumsController(AlbumsBean albumsBean) {
+    public AlbumsController(AlbumsBean albumsBean, BlobStore blobStore) {
         this.albumsBean = albumsBean;
+        this.blobStore = blobStore;
     }
-
 
     @GetMapping
     public String index(Map<String, Object> model) {
@@ -43,57 +43,52 @@ public class AlbumsController {
         return "albumDetails";
     }
 
+    /* BlobStore를 사용하여 이미지 저장 */
     @PostMapping("/{albumId}/cover")
     public String uploadCover(@PathVariable long albumId, @RequestParam("file") MultipartFile uploadedFile) throws IOException {
-        saveUploadToFile(uploadedFile, getCoverFile(albumId));
+
+        Blob blob = new Blob(
+                getCoverName(albumId),
+                uploadedFile.getInputStream(),
+                uploadedFile.getContentType()
+        );
+
+        blobStore.put(blob);
 
         return format("redirect:/albums/%d", albumId);
     }
 
+    /* BlobStore를 사용하여 이미지 저장 */
     @GetMapping("/{albumId}/cover")
     public HttpEntity<byte[]> getCover(@PathVariable long albumId) throws IOException, URISyntaxException {
-        Path coverFilePath = getExistingCoverPath(albumId);
-        byte[] imageBytes = readAllBytes(coverFilePath);
-        HttpHeaders headers = createImageHttpHeaders(coverFilePath, imageBytes);
+        Optional<Blob> maybeBlob = blobStore.get(getCoverName(albumId));
+        Blob blob = maybeBlob.orElseGet(this::defaultCover);  // null 이면 defaultCover() 실행,
+
+        byte[] imageBytes = IOUtils.toByteArray(blob.inputStream);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(blob.contentType));
+        headers.setContentLength(imageBytes.length);
 
         return new HttpEntity<>(imageBytes, headers);
     }
 
-
-    private void saveUploadToFile(@RequestParam("file") MultipartFile uploadedFile, File targetFile) throws IOException {
-        targetFile.delete();
-        targetFile.getParentFile().mkdirs();
-        targetFile.createNewFile();
-
-        try (FileOutputStream outputStream = new FileOutputStream(targetFile)) {
-            outputStream.write(uploadedFile.getBytes());
-        }
+    @DeleteMapping("/covers")
+    public String deletesCover() {
+        blobStore.deleteAll();
+        return "redirect:/albums";
     }
 
-    private HttpHeaders createImageHttpHeaders(Path coverFilePath, byte[] imageBytes) throws IOException {
-        String contentType = new Tika().detect(coverFilePath);
+    private Blob defaultCover() {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        InputStream input = classLoader.getResourceAsStream("default-cover.jpg");
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.parseMediaType(contentType));
-        headers.setContentLength(imageBytes.length);
-        return headers;
+        return new Blob("default", input, IMAGE_JPEG_VALUE);
+        // IMAGE_JPEG_VALUE : .jpg 타입을 문자열로 입력해도 되지만 문자열은 오타가 나도 확인하기 어렵기 때문에 되도록 사용하지 않는다.
+        // Springframework의 MimeTypeUtils 을 사용
     }
 
-    private File getCoverFile(@PathVariable long albumId) {
-        String coverFileName = format("covers/%d", albumId);
-        return new File(coverFileName);
-    }
-
-    private Path getExistingCoverPath(@PathVariable long albumId) throws URISyntaxException {
-        File coverFile = getCoverFile(albumId);
-        Path coverFilePath;
-
-        if (coverFile.exists()) {
-            coverFilePath = coverFile.toPath();
-        } else {
-            coverFilePath = Paths.get(getSystemResource("default-cover.jpg").toURI());
-        }
-
-        return coverFilePath;
+    private String getCoverName(@PathVariable long albumId) {
+        return format("covers/%d", albumId);
     }
 }
---
+++ src/main/java/org/superbiz/moviefun/blobstore/BlobStore.java
@@ -0,0 +1,15 @@
+package org.superbiz.moviefun.blobstore;
+
+import org.superbiz.moviefun.blobstore.Blob;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public interface BlobStore {
+
+    void put(Blob blob) throws IOException;
+
+    Optional<Blob> get(String name) throws IOException;
+
+    void deleteAll();
+}
